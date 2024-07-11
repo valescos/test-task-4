106881baedbdaea6668ac515639d4ba9621af795
valescos <valescos1986@gmail.com>
2024-07-11 13:49:15 +0700

    Connected store to AccountTable component

--- src/components/AccountItem.vue
+++ src/components/AccountItem.vue
@@ -1,5 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { Account } from "../types";
+
+defineProps<{
+  key: number;
+  account: Account;
+}>();
+</script>
 
 <template>
-  <div>AccountItem</div>
+  <div>
+    {{ account.marks }} - {{ account.category }} - {{ account.login }} -
+    {{ account.password }}
+  </div>
 </template>
--- src/components/AccountsTable.vue
+++ src/components/AccountsTable.vue
@@ -1,5 +1,12 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import AccountItem from "../components/AccountItem.vue";
+import { useAccountsStore } from "../store";
+import { storeToRefs } from "pinia";
+const accountStore = useAccountsStore();
+const { accounts } = storeToRefs(accountStore);
+</script>
 
 <template>
   <div>AccountsTable</div>
+  <AccountItem v-for="(acc, index) in accounts" :key="index" :account="acc" />
 </template>
