fa3656c36140ef26716aaeff3dcc392910bb59f1
valescos <valescos1986@gmail.com>
2024-07-11 12:52:55 +0700

    Added: dummy data

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,29 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
+import type { Account } from "../types";
 
-export const useAccountsStore = defineStore("accounts", () => {});
+const dummyaccs: Account[] = [
+  {
+    category: "LDAP",
+    login: "AAA",
+    password: "strongpassword0_0",
+  },
+  {
+    marks: [{ text: "A" }, { text: "B" }],
+    category: "LDAP",
+    login: "BBB",
+    password: "weakpassword:)",
+  },
+  {
+    marks: [{ text: "C" }, { text: "D" }],
+    category: "Локальная",
+    login: "CCC",
+    password: "safepassword!",
+  },
+];
+
+export const useAccountsStore = defineStore("accounts", () => {
+  const accounts = ref<Account[]>(dummyaccs);
+
+  return { accounts };
+});
